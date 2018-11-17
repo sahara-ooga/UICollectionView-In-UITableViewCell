b7ffed4161fbcfffda26107f0e7b2719127db6aa
yuu ogasawara <performa532@gmail.com>
2018-11-17 13:10:14 +0900

    add collectionview to tableviewcell xib and bind outlet to source code

--- UICollectionView-In-UITableViewCell/UICollectionView-In-UITableViewCell/View/AreaTableViewCell.swift
+++ UICollectionView-In-UITableViewCell/UICollectionView-In-UITableViewCell/View/AreaTableViewCell.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class AreaTableViewCell: UITableViewCell {
-
+    @IBOutlet weak var shopCollectionView: UICollectionView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
