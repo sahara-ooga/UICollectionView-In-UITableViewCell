1c7b7266b71893b85ab6f78800329b5194c23340
yuu ogasawara <performa532@gmail.com>
2018-11-17 13:00:16 +0900

    add TableViewCell and xib file

---
+++ UICollectionView-In-UITableViewCell/UICollectionView-In-UITableViewCell/View/AreaTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AreaTableViewCell.swift
+//  UICollectionView-In-UITableViewCell
+//
+//  Created by ogasawara_dev on 2018/11/17.
+//  Copyright © 2018 ogasawara_dev. All rights reserved.
+//
+
+import UIKit
+
+class AreaTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
