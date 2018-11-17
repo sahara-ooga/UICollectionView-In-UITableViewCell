adc96ae0461d767da07116b79a4a6c9dbd3557bc
yuu ogasawara <performa532@gmail.com>
2018-11-17 12:56:12 +0900

    add Collection View

---
+++ UICollectionView-In-UITableViewCell/UICollectionView-In-UITableViewCell/View/AreaCollectionView.swift
@@ -0,0 +1,21 @@
+//
+//  AreaCollectionView.swift
+//  UICollectionView-In-UITableViewCell
+//
+//  Created by ogasawara_dev on 2018/11/17.
+//  Copyright © 2018 ogasawara_dev. All rights reserved.
+//
+
+import UIKit
+
+class AreaCollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
