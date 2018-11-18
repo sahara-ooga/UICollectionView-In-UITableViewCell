548d221f33cdbad24e5d6bcfc9785fd628c430ee
yuu ogasawara <performa532@gmail.com>
2018-11-18 21:14:39 +0900

    configure datasource setting

--- UICollectionView-In-UITableViewCell/UICollectionView-In-UITableViewCell/View Controller/HomeViewController.swift
+++ UICollectionView-In-UITableViewCell/UICollectionView-In-UITableViewCell/View Controller/HomeViewController.swift
@@ -11,7 +11,10 @@ import UIKit
 class HomeViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
-    var tableViewDataSource: UITableViewDataSource!
+    lazy var shopData: ShopData? = {
+        guard let jsonData = JSONLoaderImpl.jsonData(from: "rest01") else { return nil }
+        return try? ShopData(data: jsonData)
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,10 +26,54 @@ extension HomeViewController {
     /// initial set up for table view
     func configureTableView() {
         self.register(cell: AreaTableViewCell.self, to: self.tableView)
-        self.tableView.dataSource = self.tableViewDataSource
+        self.tableView.dataSource = self
         self.tableView.delegate = self
     }
 }
 extension HomeViewController: UITableViewCellRegisterable { }
 
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    func tableView(
+        _ tableView: UITableView,
+        willDisplay cell: UITableViewCell,
+        forRowAt indexPath: IndexPath) {
+        guard let cell = cell as? AreaTableViewCell else { return }
+        cell.setCollectionViewDelegate(delegate: self)
+        cell.setCollectionViewDataSource(dataSourceDelegate: self, forRow: indexPath.row)
+    }
+    func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView
+            .dequeueReusableCell(
+                withIdentifier: "AreaTableViewCell",
+                for: indexPath) as! AreaTableViewCell
+        return cell
+    }
+}
 extension HomeViewController: UITableViewDelegate { }
+
+extension HomeViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView
+            .dequeueReusableCell(
+                withReuseIdentifier: "ShopCell",
+                for: indexPath) as! ShopCell
+        
+        let tag = collectionView.tag
+        if let shop = self.shopData?[tag].shops[indexPath.item] {
+            cell.set(shop)
+        }
+        return cell
+    }
+}
