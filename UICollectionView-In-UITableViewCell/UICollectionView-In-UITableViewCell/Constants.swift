bea09db602025b80e914fb62357b4a64f76858d5
yuu ogasawara <performa532@gmail.com>
2018-11-24 01:20:42 +0900

    add Constants enum

---
+++ UICollectionView-In-UITableViewCell/UICollectionView-In-UITableViewCell/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  UICollectionView-In-UITableViewCell
+//
+//  Created by ogasawara_dev on 2018/11/24.
+//  Copyright © 2018 ogasawara_dev. All rights reserved.
+//
+import UIKit
+
+enum Constants {
+    enum AreaTableViewCell {
+        static let cellHeight: CGFloat = 250
+        static let labelHeight = cellHeight - collectionViewHeight
+        static let collectionViewHeight: CGFloat = 230
+    }
+}
